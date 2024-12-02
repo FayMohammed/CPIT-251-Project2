ac41fda0100788b3bfad7d247d0c8f0cbdae3638
elaf-awadh-alharbi <elafutureit@gmail.com>
2024-12-02 17:45:12 +0300

    commit 3: Elaf Alharbi(push SearchService)

---
+++ Downloads/CPIT-251-Project2-master (1)/CPIT-251-Project2-master/src/project251/group5/SearchService.java
@@ -0,0 +1,30 @@
+
+package project251.group5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author elaf 2105887
+ */
+
+public class SearchService {
+    private Repository repository;
+
+    public SearchService(Repository repository) {
+        this.repository = repository;
+    }
+
+  public List<Guide> searchByKeyword(String keyword) {
+    List<Guide> results = new ArrayList<>();
+    for (Guide guide : repository.getAllGuides()) {
+        System.out.println("Checking: " + guide.getTitle());
+        if (guide.getTitle().toLowerCase().contains(keyword.toLowerCase())) {
+            results.add(guide);
+        }
+    }
+    return results;
+}
+
+}
